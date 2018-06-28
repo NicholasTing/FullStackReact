8a6fdf70f67a701136014002159a9e6d25c415f7
nicholasting92 <nicholasting92@gmail.com>
2018-06-28 15:57:15 +1000

    Accidentally used a coma instead of a full stop

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 // keys.js - figure out what set of credentials to return
 
-if (process.env,NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production'){
     // production
     module.exports = require('./prod');
-    
+
 } else {
     // development
     module.exports = require('./dev');
