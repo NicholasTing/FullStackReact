ce5b7d73ea811c0fe982faac339802b5faf32e76
nicholasting92 <nicholasting92@gmail.com>
2018-07-04 02:51:08 +1000

    Done with the front end

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 
 class Header extends Component{
 
@@ -12,7 +14,7 @@ class Header extends Component{
                 return <li><a href="/auth/google">Login With Google</a></li>;
 
             default:
-                return <li><a href="/">Logout</a></li>;
+                return <li><a href="/api/logout">Logout</a></li>;
                     
         }
     }
@@ -21,7 +23,11 @@ class Header extends Component{
         return(
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">Emaily </a>
+                    <Link to={this.props.auth ? '/surveys' : '/'} 
+                    className="left brand-logo"
+                    >
+                    Emaily 
+                    </Link>
 
                     <ul className="right">
 
---
+++ client/src/components/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Landing = () => {
+    return(
+        <div style={{textAlign:'center'}}>
+            <h1>
+                Nicholas Ting Jing Kun
+            </h1>
+
+            This is my personal website.
+        </div>
+
+    )
+}
+
+export default Landing;
