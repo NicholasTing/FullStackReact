2d9fdcaed3e6ffe64d1ab354b7d81d86975278ea
nicholasting92 <nicholasting92@gmail.com>
2018-07-05 00:43:02 +1000

    Deploy to heroku

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if(process.env.NODE_ENV === 'production'){
     // Express will serve up the index.html file
     // if it doesn't recoginze the route.
     const path = require('path');
-    app.get('*', (req,res) =>{
+    app.get('*', (req,res) =>{  
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
