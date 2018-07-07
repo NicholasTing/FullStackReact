d3ab737e7cdb61aaf2e3dfd083084d06f138a072
nicholasting92 <nicholasting92@gmail.com>
2018-07-07 18:48:31 +1000

    Save

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -9,7 +11,7 @@ module.exports = app => {
     app.post(
         '/api/surveys', requireLogin,requireCredits,
         (req,res) => {
-            const {title, subjet, body, recipients} = req.body;
+            const {title, subject, body, recipients} = req.body;
 
             const survey = new Survey({
                 title,
@@ -21,7 +23,13 @@ module.exports = app => {
                 dateSent: Date.now()
 
             });
+            
+             // Send an email
+            const mailer = new Mailer(survey, surveyTemplate(survey));
+            mailer.send();
+
         }
 
     )
+
 };
